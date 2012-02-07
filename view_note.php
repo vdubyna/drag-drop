b8fd58f2344b61540fb588221bcec575ed6de79b
unknown <klec@.(none)>
2012-02-08 00:07:28 +0200

    Note viewer

---
+++ view_note.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: klec
+ * Date: 08.02.12
+ * Time: 0:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Trust me! This is your note.";
