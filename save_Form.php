d7fb21f4d1cb8882f01fdd4e637265a008cd32bd
unknown <klec@.(none)>
2012-02-08 00:17:54 +0200

    Note viewer with htaccess reditect

--- save_Form.php
+++ save_Form.php
@@ -9,4 +9,4 @@
 //echo phpinfo();
 $url_root = $_SERVER['SERVER_NAME'];
 $note_id = round(rand(0,99999999),8);
-echo "This is <a href='http://".$url_root."/note_".$note_id."'>LINK</a> to your content";
+echo "This is <a href='http://".$url_root."/note_".$note_id.".php'>LINK</a> to your content";
